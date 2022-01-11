85956c1ae5887a0e7d57e963621b572b3f87a09e
Vanuti Souza Mascarenhas <vanutimascarenhas156@gmail.com>
2022-01-11 13:25:30 -0500

    paralelismo em docker

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -1,31 +1,44 @@
 package br.ce.wcaquino.tasks.functional;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+//import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class TasksTest {
 	
-	public WebDriver acessarAplicacao() {
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-debugging-port=9225");
-		WebDriver driver = new ChromeDriver(options);
+	public WebDriver acessarAplicacao() throws MalformedURLException {
+		
+//		Random gerador = new Random();
+//		String valorGerado = String.valueOf(gerador.nextInt(100) + 9200);
+		
+		
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments(String.format("--remote-debugging-port=%s", valorGerado));
+//		options.setHeadless(true);
+//		WebDriver driver = new ChromeDriver(options);
+		DesiredCapabilities cap = DesiredCapabilities.chrome();
+//		cap.setCapability(ChromeOptions.CAPABILITY, options);
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
 		return driver;
 	}
 	
 	@Test
-	public void deveSalvarTarefaComSucesso() {
+	public void deveSalvarTarefaComSucesso() throws MalformedURLException {
 		
 		WebDriver driver = acessarAplicacao();
 		try {
 						
 			// abrindo browser no site
-			driver.navigate().to("http://localhost:8001/tasks");
+			driver.navigate().to("http://192.168.72.130:8001/tasks");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			//clicar em Add Todo
@@ -50,13 +63,13 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemDescricao() {
+	public void naoDeveSalvarTarefaSemDescricao() throws MalformedURLException {
 		
 		WebDriver driver = acessarAplicacao();
 		try {
 						
 			// abrindo browser no site
-			driver.navigate().to("http://localhost:8001/tasks");
+			driver.navigate().to("http://192.168.72.130:8001/tasks");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			//clicar em Add Todo
@@ -78,13 +91,13 @@ public class TasksTest {
 	}
 	
 	@Test
-	public void naoDeveSalvarTarefaSemData() {
+	public void naoDeveSalvarTarefaSemData() throws MalformedURLException {
 		
 		WebDriver driver = acessarAplicacao();
 		try {
 						
 			// abrindo browser no site
-			driver.navigate().to("http://localhost:8001/tasks");
+			driver.navigate().to("http://192.168.72.130:8001/tasks");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			//clicar em Add Todo
@@ -106,13 +119,13 @@ public class TasksTest {
 	}
 
 	@Test
-	public void naoDeveSalvarTarefaComDataPassada() {
+	public void naoDeveSalvarTarefaComDataPassada() throws MalformedURLException {
 		
 		WebDriver driver = acessarAplicacao();
 		try {
 						
 			// abrindo browser no site
-			driver.navigate().to("http://localhost:8001/tasks");
+			driver.navigate().to("http://192.168.72.130:8001/tasks");
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 			
 			//clicar em Add Todo
